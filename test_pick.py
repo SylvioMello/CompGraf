5519d202491f5bd486dc34afc53187429ec310b8
SylvioMello <sylvio@poli.ufrj.br>
2023-06-15 22:23:28 -0300

    making some changes

--- test_pick.py
+++ test_pick.py
@@ -11,26 +11,20 @@ import math
 
 # Selected object
 selected = None
-
 # Set of removed objects
 removed = set()
-
 # size of cube array
 n = 3
-
 # rotation parameters
 angle = 0
 prevx = 0
 prevy = 0
-
 windowSize = (0, 0)
-
 # Variável para controlar a translação do cubo
 cube_translations = []
 # Inicializa as translações dos cubos
 for i in range(n**3):
     cube_translations.append([0, 0, 0])
-
 # Direction the cube can move
 move_left = [-40, 0, 0]
 move_right = [40, 0, 0]
@@ -38,9 +32,7 @@ move_back = [0, 40, 0]
 move_front = [0, -40, 0]
 move_up = [0, 0, 40]
 move_down = [0, 0, -40]
-
 directions = [move_left, move_right, move_back, move_front, move_up, move_down]
-
 # Target translation for each cube
 right_translation = [] 
 for i in range(n):
@@ -60,7 +52,7 @@ for i in range(n):
             if k == 2:
                 coord[2] = abs(coord[2])
             right_translation.append(coord)
-print (right_translation)
+
 class ArcBall(object):
     """Implements an arcball manipulator for specifying rotations."""
     
@@ -110,17 +102,18 @@ class ArcBall(object):
         @return: (angle,axis).
         """
         return self._rotby2vectors (self._projvector(x0,y0), self._projvector(x1,y1))
-    
 
 def rotatecallback (x, y):
-        global startx,starty,matrix
-        angle, axis = arcball.rot (startx, height - starty, x, height - y)
-        glLoadIdentity ()
-        glRotatef (degrees(angle),*axis)
-        glMultMatrixd (matrix)
-        matrix = glGetDoublev(GL_MODELVIEW_MATRIX)
-        startx,starty = x,y
-        glutPostRedisplay()
+    global startx,starty,matrix
+    angle, axis = arcball.rot (startx, height - starty, x, height - y) # retorna um angulo de rotacao e um eixo de rotacao
+    # assumindo que foi feito um gesto que comeca em startx e (height - starty)
+    # e fez um drag ate x e (height - y)
+    glLoadIdentity ()
+    glRotatef (degrees(angle),*axis) # multiplica por uma matriz de rotacao com angulo angle e no eixo axis
+    glMultMatrixd (matrix) # pega a matriz anterior e multiplica pela matriz de rotacao
+    matrix = glGetDoublev(GL_MODELVIEW_MATRIX) # retorna o resultado da multiplicacao
+    startx,starty = x,y
+    glutPostRedisplay()
 
 def draw_cube_with_image(image_path):
     glMatrixMode(GL_PROJECTION)
@@ -128,58 +121,51 @@ def draw_cube_with_image(image_path):
     glOrtho(0, windowSize[0], 0, windowSize[1], -1, 1)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-
     glBindTexture(GL_TEXTURE_2D, glGenTextures(1))
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-
-    try:
-        image = Image.open(image_path)
-        width, height = image.size
-        image_data = image.tobytes("raw", "RGB", 0, -1)
-        glEnable(GL_TEXTURE_2D)
-        glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image_data)
-        glBegin(GL_QUADS)
-        glTexCoord2f(0, 0)
-        glVertex2f(0, 0)
-        glTexCoord2f(1, 0)
-        glVertex2f(windowSize[0], 0)
-        glTexCoord2f(1, 1)
-        glVertex2f(windowSize[0], windowSize[1])
-        glTexCoord2f(0, 1)
-        glVertex2f(0, windowSize[1])
-        glEnd()
-        glDisable(GL_TEXTURE_2D)
-    except IOError:
-        print(f"Failed to open the image file: {image_path}")
-
+    image = Image.open(image_path)
+    width, height = image.size
+    image_data = image.tobytes("raw", "RGB", 0, -1)
+    glEnable(GL_TEXTURE_2D)
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image_data)
+    glBegin(GL_QUADS)
+    glTexCoord2f(0, 0)
+    glVertex2f(0, 0)
+    glTexCoord2f(1, 0)
+    glVertex2f(windowSize[0], 0)
+    glTexCoord2f(1, 1)
+    glVertex2f(windowSize[0], windowSize[1])
+    glTexCoord2f(0, 1)
+    glVertex2f(0, windowSize[1])
+    glEnd()
+    glDisable(GL_TEXTURE_2D)
 
 def loadTexture (filename):
     "Loads an image from a file as a texture"
-
     # Read file and get pixels
     imagefile = Image.open(filename)
     sx,sy = imagefile.size[0:2]
     global pixels
     pixels = imagefile.convert("RGBA").tobytes("raw", "RGBA", 0, -1)
-
     # Create an OpenGL texture name and load image into it
     image = glGenTextures(1)
     glBindTexture(GL_TEXTURE_2D, image)  
     glPixelStorei(GL_UNPACK_ALIGNMENT,1)
     glTexImage2D(GL_TEXTURE_2D, 0, 3, sx, sy, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels)
-    
     # Set other texture mapping parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
-    
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL) # mudar GL_DECAL para GL_MODULATE?
+    # tem que ligar a iluminacao??
     # Return texture name (an integer)
-
     return image
 
+# fazer quatro faces com as flechas e duas faces brancas
+# tem que ser resolvivel...
+# se tiver algum bloco no caminho de outro, esse outro precisa ficar parado
 def drawCube(size):
     glBegin(GL_QUADS)  # Start Drawing The Cube
 
@@ -259,9 +245,7 @@ def draw_scene(flatColors=False):
     glTranslatef(0, 0, -3)
     glMultMatrixd (matrix)
     glRotatef(-80, 1, 0, 0)
-    
     size = 1 / n
-
     for i in range(n):
         x = i - (n - 1) / 2
         for j in range(n):
@@ -277,10 +261,8 @@ def draw_scene(flatColors=False):
                 glPushMatrix()
                 glTranslatef(x * size + cube_translations[name][0], y * size + cube_translations[name][1], z * size + cube_translations[name][2])
                 glutSolidCube(size * 0.8)
-
                 # Texture initialization
-                textureId = loadTexture("arrow2.jpeg") 
-
+                loadTexture("arrow2.jpeg") 
                 # Rotate cube to align with the x-axis
                 x_rot, y_rot, z_rot = right_translation[name][0] / 40, right_translation[name][1] / 40, right_translation[name][2] / 40
                 if x_rot == 1.0:
@@ -297,15 +279,12 @@ def draw_scene(flatColors=False):
                     glRotatef(-90, 1, 0 , 0)
                 drawCube(size * 0.401)
                 glPopMatrix()
-
     if len(removed) == n**3:
-        draw_cube_with_image("Trab2/youwin.png")  # Replace with the actual image path
+        draw_cube_with_image("youwin.png")  # Replace with the actual image path
 
 def display():
     draw_scene()
     glutSwapBuffers()
-
-    
 
 def init():
     glClearColor(0.0, 0.0, 0.0, 0.0)
@@ -362,21 +341,13 @@ def mousePressed(button, state, x, y):
     elif state == GLUT_UP:
         prevx, prevy = x, y
         coordinates = pick(x, y)
-        print(coordinates)
         selected = (coordinates[0] * n + coordinates[1]) * n + coordinates[2]
-        print(selected)
-        print(right_translation[selected])
-        
-        
         if selected >= 0:
-
             neighbor = selected + (right_translation[selected][0]/40) * 9 + (right_translation[selected][1]/40) * 3 + (right_translation[selected][2]/40) * 1
-            print(neighbor)
             pass_through = False
             if coordinates[0] != 1 or coordinates[1] != 1 or coordinates[2] != 1:
                 if coordinates[0] == 0:
                     if  right_translation[selected] == [-40, 0, 0]:
-                        print('oi')
                         pass_through = True
                 if coordinates[0] == 2:
                     if  right_translation[selected] == [40, 0, 0]:
@@ -400,14 +371,10 @@ def mousePressed(button, state, x, y):
                 # Configura o movimento gradual do cubo
                 start_translation = cube_translations[selected_cube].copy()
                 target_translation = right_translation[selected_cube].copy()
-                
-
                 total_frames = 150  # Número total de quadros para completar o movimento
                 frame = 0
-
                 def lerp(a, b, t):
                     return np.array(a) + (np.array(b) - np.array(a)) * t
-
                 def update_translation(arg=None):
                     nonlocal frame
                     if frame >= total_frames/10:
@@ -421,7 +388,6 @@ def mousePressed(button, state, x, y):
                         frame += 2
                         glutTimerFunc(16, update_translation, None)  # Aguarda 16ms (aproximadamente 60 quadros por segundo) e chama novamente a função
                     glutPostRedisplay()
-
                 # Inicia o movimento
                 update_translation()
             else: 
@@ -429,17 +395,10 @@ def mousePressed(button, state, x, y):
                 # Configura o movimento gradual do cubo
                 start_translation = cube_translations[selected_cube].copy()
                 target_translation = [x/500 for x in right_translation[selected_cube].copy()]
-
-
-                print(target_translation)
-          
-
                 total_frames = 3  # Número total de quadros para completar o movimento
                 frame = 0
-
                 def lerp(a, b, t):
                     return np.array(a) + (np.array(b) - np.array(a)) * t
-
                 def update_translation(arg=None):
                     nonlocal frame
                     if frame >= total_frames:
@@ -451,18 +410,10 @@ def mousePressed(button, state, x, y):
                         cube_translations[selected_cube] = lerp(start_translation, target_translation, t)
                         frame += 1
                         glutTimerFunc(4, update_translation, None)  # Aguarda 16ms (aproximadamente 60 quadros por segundo) e chama novamente a função
-
-
-
                     glutPostRedisplay()
-
-
                 # Inicia o movimento
                 update_translation()
-                print(cube_translations[selected_cube])
                 cube_translations[selected_cube] = [0,0,0]
-            
-        
     glutPostRedisplay()
 
 def main():
